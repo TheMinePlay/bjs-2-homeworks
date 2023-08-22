3ec85258aead491fe523b81aa6d719a2b6ca34eb
ZheMinePlay <egor.reznikov.02@list.ru>
2023-08-22 20:08:16 +0700

    Домашнее задание к лекции 3. «Массивы»

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,30 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    if (arr1 instanceof Array && arr2 instanceof Array) {
+        if (arr1.length !== arr2.length) {
+            return false;
+        }
+
+        for (var i = 0; i < arr1.length; i++)
+        {
+            if (!compareArrays(arr1[i], arr2[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    return arr1 === arr2;
+};
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  let filteredUsers = users.filter((user) => user.gender === gender);
+
+  if (filteredUsers.length === 0) {
+    return 0;
+  };
+
+  let totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+  let averageAge = totalAge / filteredUsers.length;
+  return averageAge;
+};
